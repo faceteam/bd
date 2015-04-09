40f7b29a687d874580c53e8a7e5b5fdc1dabacd1
zhangjie <luoyetx@gmail.com>
2015-04-09 19:06:40 +0800

    add headers for requests

--- bd/spiders/baidu.py
+++ bd/spiders/baidu.py
@@ -12,13 +12,22 @@ class BaiduSpider(scrapy.Spider):
         baidu pictures for face project
     """
     name = 'Baidu'
+
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Host': 'h.hiphotos.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36',
+    }
+
+    #url_pattern = "http://image.baidu.com/i?tn=resultjsonavatarnew&ie=utf-8&word={0}&cg=star&pn={0}&rn={1}"
     url_pattern = "http://image.baidu.com/data/star/listjson?pn={0}&rn={1}&name={2}"
 
     def start_requests(self):
         for name in PeopleNames:
             for page in range(1, Pages+1):
                 url = self.url_pattern.format(page, 100, name)
-                yield Request(url)
+                yield Request(url, headers=self.headers)
 
     def parse(self, response):
         data = json.loads(response.body)
